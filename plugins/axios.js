641849e4e999087b6092aafec80e4e93b89694fe
kenncapella <kenn@capellbpo.coma>
2023-09-14 20:58:30 +0800

    Removed ~/plugins/axios

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export default ({ app }, inject) => {
-  const axiosInstance = axios.create({
-    baseURL: 'https://api.openai.com/v1', // Replace with the actual ChatGPT API endpoint
-  });
+// export default ({ app }, inject) => {
+//   const axiosInstance = axios.create({
+//     baseURL: 'https://api.openai.com/v', // Replace with the actual ChatGPT API endpoint
+//   });
 
-  inject('axios', axiosInstance);
-};
+//   inject('axios', axiosInstance);
+// };
